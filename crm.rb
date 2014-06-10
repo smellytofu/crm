3c1d0f130e855b2a12bdc2979ded05562ba02371
Matthew Wong <wong.mat@gmail.com>
2014-06-10 11:10:11 -0400

    added contacts

--- crm.rb
+++ crm.rb
@@ -4,3 +4,19 @@ get '/' do
 	@crmAppName = "My CRM"
 	erb :index
 end
+
+get '/contacts' do
+	erb :contacts
+end
+
+get '/contacts/new' do
+	#
+end
+
+get '/contacts/:id' do
+	#
+end
+
+get '/contacts/:id/edit' do
+	#
+end
